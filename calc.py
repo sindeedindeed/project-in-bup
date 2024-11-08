84d13a71570f7d370e2233d0ceab3402bc2088e4
Tashmeah03 <faizatashmeah@gmail.com>
2024-11-09 00:22:59 +0600

    Update calc.py
    Added intergar type

--- calc.py
+++ calc.py
@@ -4,23 +4,25 @@ def add(num1, num2):
 def subtract(num1, num2):
     return num1 - num2
 
-def multiplication(num1,num2):
-    print(num1*num2)  
+def multiplication(num1, num2):
+    return int(num1 * num2)
 
-def division(num1,num2):
-    print(num1/num2)       
+def division(num1, num2):
+    return int(num1 / num2)
 
-x = input('Enter first operand: ')
-y = input('Enter second operand: ')
+x = int(input('Enter first operand: '))
+y = int(input('Enter second operand: '))
+
 result = add(x, y)
 print("Addition:", result)
 
-result = subtract(5, 3)
+result = subtract(x, y)
 print("Subtraction:", result)
 
-result = multiplication(5, 3)
-print("Subtraction:", result)
+result = multiplication(x, y)
+print("Multiplication:", result)
 
-result = division(5, 3)
-print("Subtraction:", result)
+result = division(x, y)
+print("Division:", result)
+
 
