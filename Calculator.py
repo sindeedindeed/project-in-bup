9dc1eaedaffbd01d5fd410ca1249ce7e2b53d6ce
Mahdi Hasan <145856057+eeemrann@users.noreply.github.com>
2024-11-08 23:41:37 +0600

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+result = add(5, 3)
+print("Addition:", result)
+
+result = subtract(5, 3)
+print("Subtraction:", result)
+
+print(yaay done)
