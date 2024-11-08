b9682bd1ff90ff6103777f8bed0f905402913f4a
Mahdi Hasan <145856057+eeemrann@users.noreply.github.com>
2024-11-09 00:08:03 +0600

    Add files via upload

--- calc.py
+++ calc.py
@@ -4,10 +4,21 @@ def add(num1, num2):
 def subtract(num1, num2):
     return num1 - num2
 
+def multiplication(num1,num2):
+    print(num1*num2)  
+
+def division(num1,num2):
+    print(num1/num2)       
+
 result = add(5, 3)
 print("Addition:", result)
 
 result = subtract(5, 3)
 print("Subtraction:", result)
 
-print(yaay done)
+result = multiplication(5, 3)
+print("Subtraction:", result)
+
+result = division(5, 3)
+print("Subtraction:", result)
+
